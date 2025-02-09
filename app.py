c9c20e3b7169cbd7469be435f3942cc306e999ad
santiago cano <markpolo137@gmail.com>
2025-02-08 20:51:53 -0500

    Ajustes del puerto

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -51,3 +52,7 @@ def get_users():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port)
